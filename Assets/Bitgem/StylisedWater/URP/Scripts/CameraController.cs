a9b6ed38b643f6474a63676d2e9891b327c7f424
Aranvihn Clark <aranvihn.clark@gmail.com>
2023-11-13 17:43:04 -0500

    Update - Added camera rotation functionality

--- Assets/Bitgem/StylisedWater/URP/Scripts/CameraController.cs
+++ Assets/Bitgem/StylisedWater/URP/Scripts/CameraController.cs
@@ -1,17 +1,77 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class CameraController : MonoBehaviour
 {
     // Grabs the player's position.
-    [SerializeField] Transform playerTransform;    
-    
+    [SerializeField] private Transform playerTransform;
+    [SerializeField] private float rotationSpeed;
+    private bool isTurning;
+    private int turnDirection;
+    private float previousRotationAngle;
+    private float changeInRotation;
+    private float totalRotationCompleted;
+
+    private void Start()
+    {
+        isTurning = false;
+        
+        // Possibly removing the SerializeField above and setting it to the below as a static.
+        // For now, until I'm done testing, this will be here as a comment.
+        // rotationSpeed = 300f;
+
+        previousRotationAngle = transform.eulerAngles.y;
+        changeInRotation = 0;
+        totalRotationCompleted = 0;
+    }
 
     // Update is called once per frame
     private void Update()
     {
+        if (!isTurning)
+        {
+            changeInRotation = 0;
+            totalRotationCompleted = 0;
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                isTurning = true;
+                PlayerMovement.canMove = false;
+                turnDirection = 1;
+            } else if (Input.GetKeyDown(KeyCode.Q))
+            {
+                isTurning = true;
+                PlayerMovement.canMove = false;
+                turnDirection = -1;
+            }
+        }
+
+        if (isTurning)
+        {
+            CheckRotation();
+        }
+
         // Update's the camera's position to mirror the player's location.
-        transform.position = new Vector3(playerTransform.position.x, playerTransform.position.y, transform.position.z);
+        transform.position = new Vector3(playerTransform.position.x, transform.position.y, playerTransform.position.z);
+    }
+
+    private void CheckRotation()
+    {
+        changeInRotation = turnDirection * rotationSpeed * Time.deltaTime;
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y + changeInRotation, transform.eulerAngles.z);
+
+        totalRotationCompleted += changeInRotation;
+        if (Mathf.Abs(totalRotationCompleted) >= 90)
+        {
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x, previousRotationAngle + (turnDirection * 90), transform.eulerAngles.z);
+            isTurning = false;
+            PlayerMovement.canMove = true;
+            previousRotationAngle = transform.eulerAngles.y;
+        }
+
     }
 }
